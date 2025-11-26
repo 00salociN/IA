71a220e55228403c3ec062ed9835d128b75c4ea6
00salociN <cleristonopato@outlook.com>
2025-11-26 07:58:55 -0300

    Add files via upload

---
+++ genia/ngrams.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.util import ngrams
+
+'''
+sentence = 'Estou aprendendo IA'
+tokens = word_tokenize(sentence)
+trigrams = list(ngrams(tokens, 2))
+
+print(trigrams) '''
+
+
