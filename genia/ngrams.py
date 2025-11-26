4a7d5ec8fc8e79065c70e3661ceabe944f9b3208
00salociN <cleristonopato@outlook.com>
2025-11-26 14:09:29 -0300

    Enhance ngrams.py to include unigrams and bigrams
    Added functionality to generate unigrams, bigrams, and trigrams from a new sample text. Included necessary NLTK resource downloads.

--- genia/ngrams.py
+++ genia/ngrams.py
@@ -1,6 +1,9 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
+# Baixar os recursos necessários
+nltk.download('punkt')
+nltk.download('punkt_tab')   # Necessário em versões novas do NLTK
 
 '''
 sentence = 'Estou aprendendo IA'
@@ -8,5 +11,19 @@ tokens = word_tokenize(sentence)
 trigrams = list(ngrams(tokens, 2))
 
 print(trigrams) '''
+
+texto_simples = 'eu estou aprendendo NLP (Natural Language Processing)'
+tokens = word_tokenize(texto_simples)
+
+unigrams = list(ngrams(tokens, 1))
+print('Unigrams: \n', unigrams)
+
+bigrams = list(ngrams(tokens, 2))
+print('Bigrams: \n', bigrams)
+
+trigrams = list(ngrams(tokens, 3))
+print('Trigrams: \n', trigrams)
+
+
 
 
